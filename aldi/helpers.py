26cb51d4f971279e56674d5a5851140018102ed2
Heather Doig <hdoi5324@uni.sydney.edu.au>
2025-02-11 17:32:00 +1100

    random seed for split dataset

--- aldi/helpers.py
+++ aldi/helpers.py
@@ -330,15 +330,21 @@ def split_train_data(cfg):
     for name in cfg.DATASETS.TRAIN:
         if '_split_' in name:
             ds_name, num_split = name.split('_split_')
-            labelled, unlabelled = split_dataset_labelled_unlabelled(ds_name, int(num_split), filter_empty=cfg.DATALOADER.FILTER_EMPTY_ANNOTATIONS)
-            new_ds.append(labelled)
-            cfg.DATASETS.UNLABELED.append(unlabelled)
+            labelled, unlabelled = split_dataset_labelled_unlabelled(
+                ds_name,
+                int(num_split),
+                filter_empty=cfg.DATALOADER.FILTER_EMPTY_ANNOTATIONS,
+                seed=cfg.SEED)
+            if labelled is not None:
+                new_ds.append(labelled)
+            if unlabelled is not None:
+                cfg.DATASETS.UNLABELED.append(unlabelled)
         else:
             new_ds.append(name)
     cfg.DATASETS.TRAIN = new_ds
     return cfg
 
-def split_dataset_labelled_unlabelled(dataset_name, num_labelled, filter_empty=True):
+def split_dataset_labelled_unlabelled(dataset_name, num_labelled, filter_empty=True, seed=0):
     # get metadata
     metadata = MetadataCatalog.get(dataset_name)
 
@@ -348,14 +354,18 @@ def split_dataset_labelled_unlabelled(dataset_name, num_labelled, filter_empty=T
         dataset_dicts = filter_images_with_only_crowd_annotations(dataset_dicts)
     num_instances = len(dataset_dicts)
     indices = np.arange(num_instances)
+    np.random.seed(seed)
     np.random.shuffle(indices)
     labelled_indices = indices[:num_labelled]
     unlabelled_indices = indices[num_labelled:]
 
     #register datasets
     register_coco_instances_with_split(f"{dataset_name}_labelled", metadata, metadata.json_file, metadata.image_root, labelled_indices, filter_empty)
-    register_coco_instances_with_split(f"{dataset_name}_unlabelled", metadata, metadata.json_file, metadata.image_root, unlabelled_indices, filter_empty)
-    return f"{dataset_name}_labelled", f"{dataset_name}_unlabelled"
+    if len(unlabelled_indices) > 0:
+        register_coco_instances_with_split(f"{dataset_name}_unlabelled", metadata, metadata.json_file, metadata.image_root, unlabelled_indices, filter_empty)
+        return f"{dataset_name}_labelled", f"{dataset_name}_unlabelled"
+    else:
+        return f"{dataset_name}_labelled", None
 
 def register_coco_instances_with_split(name, metadata, json_file, image_root, indices, filter_empty):
     DatasetCatalog.register(name, lambda: load_coco_json_with_split(json_file, image_root, metadata.name, indices, filter_empty))
@@ -367,5 +377,5 @@ def load_coco_json_with_split(json_file, image_root, parent_name, indices, filte
     if filter_empty:
         dataset_dicts = filter_images_with_only_crowd_annotations(dataset_dicts)
     logger = logging.getLogger("detectron2")
-    logger.info("aldi.helpers: Splitting off {} images of {} images in COCO format from {}".format(len(indices), len(dataset_dicts), json_file))
+    logger.info("aldi.helpers: Splitting off {} images of {} images in COCO format from {}: first 10 indices {}".format(len(indices), len(dataset_dicts), json_file, indices[:min(len(indices), 10)]))
     return [dataset_dicts[index] for index in indices]
